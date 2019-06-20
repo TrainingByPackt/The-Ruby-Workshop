f9c99776d014d559fb116d7bee646db1a9df78f7
Snehal Tambe <45845632+packtsnehalt@users.noreply.github.com>
2019-06-20 14:21:39 +0530

    Add files via upload

---
+++ chapter1/Exercise 12.rb
@@ -0,0 +1,19 @@
+> title = "My Favorite book is Java Fundamentals" 
+
+ => "My Favorite book is Java Fundamentals" 
+
+ 
+
+> title["Java"] = "Ruby" 
+
+ => "Ruby" 
+
+ 
+
+> title 
+
+ => "My Favorite book is Ruby Fundamentals" 
+
+
+ 
+ 
