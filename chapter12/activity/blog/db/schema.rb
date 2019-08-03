7b6f39e156adc0ba01286c34fff56330ebf25257
akshatpaul <akshatpaul@gmail.com>
2019-08-03 05:34:12 +0530

    activity code to deploy application

--- chapter12/activity/blog/db/schema.rb
+++ chapter12/activity/blog/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20190802222140) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
     t.text "body"
