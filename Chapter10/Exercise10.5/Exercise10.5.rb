d88fe75c73472c6b4dc1ca55ad50cd30985bc229
Aditya <adityaks95@gmail.com>
2019-09-23 11:46:56 +0530

    removed

--- Chapter10/Exercise10.5/Exercise10.5.rb
+++
@@ -1,7 +0,0 @@
-require 'net/http'
-
-uri = URI('https://www.packtpub.com')
-
-response = Net::HTTP.get_response(uri)
-puts response.code
-puts response.body
--- Chapter10/Exercise10.6/Exercise10.6.rb
+++
@@ -1,5 +0,0 @@
-require 'httparty'
-
-res = HTTParty.post("https://my-json-server.typicode.com/typicode/demo/posts", body: { id: 5, title: "Ruby Fundamentals" })
-puts res.code
-puts res.body
